dbe7a8176bb4f9db13c62c558fa02a698d1dddef
Núbia Torres <nubia.oliveiras06@gmail.com>
2026-04-17 17:28:12 -0300

    Correção

--- public/script.js
+++ public/script.js
@@ -1,36 +1,53 @@
 var nome = prompt("Digite seu nome: ");
 var renda = prompt("Digite sua renda: ");
-var qtd_despesas = prompt("Digite a quantidade de despesas: ");
+var mensagem = "";
 var soma_despesas = 0;
 var sobra = 0;
 
-while (Number.isNaN(renda)) {
-    if (isNaN(renda)){
-        var renda = prompt("Digite sua renda: ");
-    } 
+while (isNaN(renda) || renda === "") {
+    renda = prompt("Renda deve ser um valor numérico. Digite sua renda: ");
+}
+renda = Number(renda);
+
+var qtd_despesas = Number(prompt("Digite a quantidade de despesas: "));
+
+while (isNaN(qtd_despesas) || qtd_despesas === "") {
+    qtd_despesas = Number(prompt("Digite um número válido de despesas: "));
+}
+
+if (qtd_despesas < 1){
+    qtd_despesas = 1;
+} else if (qtd_despesas > 5){
+    qtd_despesas = 5;
 }
 
 for (let i = 1; i <= qtd_despesas; i++){
-    var despesa = Number(prompt("Digite a despesa " + i + ":"));
+    var despesa = prompt("Digite a despesa " + i + ":");
+
+    while (isNaN(despesa) || despesa === "") {
+        despesa = prompt("Valor inválido. Digite a despesa " + i + ":");
+    }
+
+    despesa = Number(despesa);
     soma_despesas += despesa;
 }
 
 if (soma_despesas > renda){
-    console.log("⚠️ Atenção: você gastou mais do que ganhou.");
+    mensagem = "⚠️ Atenção: você gastou mais do que ganhou.";
 } else {
     sobra = renda - soma_despesas;
     if (sobra >= renda * 0.30){
-        console.log("Valor de sobra é R% " + sobra + " ✅ Ótimo: boa margem de sobra.");
+        mensagem = "✅ Ótimo: boa margem de sobra.";
     } else {
-        console.log("Valor de sobra é R% " + sobra + " 🙂 Ok: dá para melhorar a sobra.");
+        mensagem = "🙂 Ok: dá para melhorar a sobra.";
     }
 }
 
-console.log("Usuário " + nome + " com renda de R$" + Number(renda).toFixed(2) + " mensais, e R$ " +  
-Number(soma_despesas).toFixed(2) + " em despesas mensais. Com sobra de R$" + Number(sobra).toFixed(2));
+let resposta = "Usuário(a): " + nome +
+    "\nRenda: R$ " + renda.toFixed(2) +
+    "\nDespesas: R$ " + soma_despesas.toFixed(2) +
+    "\nSobra: R$ " + sobra.toFixed(2) +
+    "\nClassificação: " + mensagem;
 
-alert("Usuário " + nome + " com renda de R$" + Number(renda).toFixed(2) + " mensais, e R$ " +  
-Number(soma_despesas).toFixed(2) + " em despesas mensais. Com sobra de R$" + Number(sobra).toFixed(2))
-
-
-
+alert(resposta);
+console.log(resposta);
